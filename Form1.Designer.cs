5e8669b5d03d982cd1ed93590427eed4cf041124
fixedNick <kkos44911@gmail.com>
2023-03-25 23:00:49 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ChemicalСompoundRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.label1.Size = new System.Drawing.Size(548, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LOAD YOUR GRAPH BY DRAG&DROP IMAGE ON ANY PLACE";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(794, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(290, 348);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(794, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(290, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "COORDINATES";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 348);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(12, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(246, 71);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "PARSE DATA FROM GRAPH";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(264, 392);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 45);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Enter width step";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(264, 440);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(158, 23);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "1";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 472);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChemicalСompoundRecognition
+{
+    public partial class Form1 : Form
+    {
+        private static Bitmap? ActiveGraph = null;
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += form_DragEnter;
+            this.DragDrop += form_DragDrop;
+
+        }
+
+        private void form_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void form_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files?.Length <= 0 || new[] { files[0].Split('.').Last() }.Any(end =>
+            {
+                var validFormats = new[] { "png", "jpg", "jpeg", "gif", "bmp" };
+                if (validFormats.Where(f => f.ToLower().Equals(end.ToLower())).FirstOrDefault() == null)
+                    return false;
+                return true;
+            }) == false) return;
+
+            var firstFile = files[0];
+            Bitmap fileBitmap = new Bitmap(firstFile);
+            pictureBox1.Image = fileBitmap;
+            ActiveGraph = fileBitmap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //for(var y = 0; y < ActiveGraph.Height; y++)
+            //{
+            //    var p = ActiveGraph.GetPixel(250, y);
+            //    if(p.ToArgb() == Color.Black.ToArgb())
+            //        MessageBox.Show("Черный тут: " + y);
+            //}
+
+            //return;
+            if (ActiveGraph == null || !int.TryParse(textBox3.Text, out int step) || step <= 0) return;
+
+            GetCoordinates(step);
+            GetMatrix();
+        }
+
+        private void GetCoordinates(int step)
+        {
+            var coordinates = new List<Point>();
+            var width = ActiveGraph.Width;
+            var height = ActiveGraph.Height;
+
+            for (var x = 0; x < width; x += step)
+            {
+                for (var y = 0; y < height; y++)
+                {
+                    var pixel = ActiveGraph.GetPixel(x, y);
+                    if (pixel.R <= 120 && pixel.G <= 120 && pixel.B <= 120) { coordinates.Add(new Point(x, y)); break; }
+                }
+            }
+
+            textBox1.Clear();
+            coordinates.ForEach(point => textBox1.AppendText($"[{point.X},{point.Y}]" + Environment.NewLine));
+        }
+
+        private void GetMatrix()
+        {
+            var matrix = new List<PixelColor>();
+            var width = ActiveGraph.Width;
+            var height = ActiveGraph.Height;
+
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++)
+                {
+                    var pixel = ActiveGraph.GetPixel(x, y);
+                    if (pixel.R <= 120 && pixel.G <= 120 && pixel.B <= 120)
+                        matrix.Add(new PixelColor(x, y, 1));
+                    else matrix.Add(new PixelColor(x, y, 0));
+                }
+            }
+
+            PixelColor.SavePixelColorsIntoFile(matrix);
+        }
+    }
+
+    struct PixelColor
+    {
+        public int X;
+        public int Y;
+        public int Color;
+        public PixelColor(int x, int y, int c)
+        {
+            X = x;
+            Y = y;
+            Color = c;
+        }
+
+        public static void SavePixelColorsIntoFile(List<PixelColor> collection)
+        {
+            if (collection?.Count == 0) return;
+
+            string fileText = string.Empty;
+
+            var currentLine = collection[0].Y;
+            collection.ForEach(pixel => {
+                if (currentLine != pixel.Y)
+                {
+                    fileText += Environment.NewLine;
+                    currentLine++;
+                }
+
+                fileText += pixel.Color;
+            });
+
+            File.WriteAllText("graphMatrix.txt", fileText);  
+        }
+    }
+}
